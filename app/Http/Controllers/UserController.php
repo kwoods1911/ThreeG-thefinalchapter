27dcaa7dab80c9ed95f3e31052e1e73082c402db
Khari <khariwoods3@gmail.com>
2023-05-04 00:39:28 -0300

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function createUser(Request $request){
+        dd($request);
+        return User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+        
+    }
+
+    
+}
---
+++ routes/api.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Models\User;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+Route::post('/createuser',function(Request $request){
+    // get post response input
+    //post information to users table.
+    $data = $request->all();
+
+    // Retrieve flight by name or create it with the name, delayed, and arrival_time attributes...
+$res = User::where('email',$data['email'])->first();
+
+if ($data['password'] !== $data['confirmpassword']) 
+                return response('Password do not match', 500)
+                        ->header('Content-Type', 'json');
+
+if($res)
+    return response('Email already exists!', 500)
+                  ->header('Content-Type', 'json');
+
+
+                  
+    $user = User::Create([
+        'name'=> $data['name'], 
+        'email' => $data['email'],
+        'password'=> $data['password']
+    ]);
+    $user->save();
+    return response('Account Successfully Created!', 200)
+                  ->header('Content-Type', 'json');
+});
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
